8e53d5566c13e042d3532f3f8db182a447323055
Ritik Kumar <kr2060398@gmail.com>
2026-01-25 09:56:36 +0530

    MNT

--- script.js
+++ script.js
@@ -575,7 +575,7 @@ window.addEventListener("load", function () {
 // ==========================================
 document.addEventListener("DOMContentLoaded", function () {
   setTimeout(() => {
-    showNotification("Welcome to Helix Clinic! Book your test today. 🏥", "success", 5000);
+    showNotification("Welcome to Helix Clinic! Book your test today. 🏥", "success", 6000);
   }, 2000);
 });
 
